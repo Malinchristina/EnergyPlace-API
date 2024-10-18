d0e975f07ed4d93300ccc77ea09acd14d624a564
Malinchristina <malin_christina@yahoo.se>
2024-10-18 16:17:43 +0200

    create profiles app

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -48,6 +48,9 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
+
+    # my apps
+    'profiles',
 ]
 
 MIDDLEWARE = [
---
+++ profiles/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Profile(models.Model):
+    owner = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    image = models.ImageField(
+        upload_to='images/', default='../default_profile_yvwtw0')
+    content = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f"{self.owner}'s profile"
+
+    
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(owner=instance)
+
+post_save.connect(create_profile, sender=User)

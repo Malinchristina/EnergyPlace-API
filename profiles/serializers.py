596639dd71bdc89564941d5839556a6f578c5033
Malinchristina <malin_christina@yahoo.se>
2024-10-19 11:24:13 +0200

    add, views, urls and serializers for profiles

---
+++ profiles/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+from .models import Profile
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+
+    class Meta:
+        model = Profile
+        fields = [
+            'id',
+            'owner',
+            'name',
+            'image',
+            'content',
+            'created_at',
+            'updated_at',
+        ]

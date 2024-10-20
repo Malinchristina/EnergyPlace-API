5266abdec30cdfac2c23981639729a639f53fdc1
Malinchristina <malin_christina@yahoo.se>
2024-10-20 14:03:16 +0200

    add list and detail views to comments

--- comments/views.py
+++ comments/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from drf_api.permissions import IsOwnerOrReadOnly
+from .models import Comment
+from .serializers import CommentSerializer, CommentDetailSerializer
 
 # Create your views here.
+
+class CommentList(generics.ListCreateAPIView):
+    """
+    List and create view for Comment model.
+    """
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Comment.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Edit and delete view for Comment model.
+    """
+    serializer_class = CommentDetailSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+    queryset = Comment.objects.all()

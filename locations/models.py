417a7cadaddc4004c5292aab4b1356591d8219b5
Malinchristina <malin_christina@yahoo.se>
2024-10-20 20:53:17 +0200

    add model for location and migrate

--- locations/models.py
+++ locations/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django_countries.fields import CountryField
 
 # Create your models here.
+
+class Location(models.Model):
+    """
+    Location model. Name is specific location name (e.g., city or landmark). 
+    """
+    name = models.CharField(max_length=100)
+    country = CountryField()
+
+    def __str__(self):
+        return f'{self.name}, {self.country.name}'

4ed9408667c21ff785035954a3391cd7768e0283
Malinchristina <malin_christina@yahoo.se>
2024-10-20 20:54:40 +0200

    add urls for locations

---
+++ locations/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from locations import views
+
+urlpatterns = [
+    path('locations/', views.LocationList.as_view(), name='locations'),
+    path('locations/<int:pk>/', views.LocationDetail.as_view(), name='location'),
+]

bba184282effaf69bcb150f754fae386bcd0c758
Malinchristina <malin_christina@yahoo.se>
2024-10-20 13:19:22 +0200

    set up and migrate comments app

---
+++ comments/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.contrib.auth.models import User
+from posts.models import Post
+
+# Create your models here.
+
+class Comment(models.Model):
+    """
+    Comment model, references User and Post models.
+    """
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f'{self.id} {self.content}'
--- drf_api/settings.py
+++ drf_api/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
     # my apps
     'profiles',
     'posts',
+    'comments',
 ]
 
 MIDDLEWARE = [

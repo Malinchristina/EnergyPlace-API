9f5206ea61758e22732914d8b1a5bc01603edc9e
Malinchristina <malin_christina@yahoo.se>
2024-10-22 07:13:43 +0200

    add serializers and views for categories

--- categories/views.py
+++ categories/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Category
+from .serializers import CategorySerializer
 
 # Create your views here.
+
+class CategoryList(generics.ListCreateAPIView):
+    """
+    Category list view.
+    """
+    serializer_class = CategorySerializer
+    queryset = Category.objects.all()
+
+
+class CategoryDetail(generics.RetrieveUpdateAPIView):
+    """
+    Retrieve, update category.
+    """
+    serializer_class = CategorySerializer
+    queryset = Category.objects.all()

9c061dfb8409fc916ca5c34b8f4745330907b299
Malinchristina <malin_christina@yahoo.se>
2024-10-27 19:18:06 +0100

    fix(settings): update ALLOWED_HOSTS to use ALLOWED_HOST environment variable

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -61,7 +61,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = 'DEV' in os.environ
 
 ALLOWED_HOSTS = [
-    os.environ.get('ALLOWED_HOSTS'),
+    os.environ.get('ALLOWED_HOST'),
     'localhost',
 ]
 

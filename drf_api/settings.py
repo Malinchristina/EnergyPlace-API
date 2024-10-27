a164ea2a6541fd9f91d1c258107cc15fe7fd3778
Malinchristina <malin_christina@yahoo.se>
2024-10-27 18:58:55 +0100

    chore(settings): add CSRF_TRUSTED_ORIGINS for trusted hosts

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -65,6 +65,12 @@ ALLOWED_HOSTS = [
     'localhost',
 ]
 
+CSRF_TRUSTED_ORIGINS = [
+    'http://localhost:8000',
+    'http://127.0.0.1:8000',
+    'energyplace-be-f35f3084f662.herokuapp.com'
+]
+
 
 # Application definition
 

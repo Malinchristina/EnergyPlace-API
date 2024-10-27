d3ee8abd28fc43795e5487c2ebf5cc6ddfbe59a0
Malinchristina <malin_christina@yahoo.se>
2024-10-27 10:56:05 +0100

    feat(category): add custom message for empty category list response

--- categories/views.py
+++ categories/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from rest_framework.response import Response
 from .models import Category
 from .serializers import CategorySerializer
 from django_filters.rest_framework import DjangoFilterBackend
@@ -14,6 +15,15 @@ class CategoryList(generics.ListCreateAPIView):
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['name']
 
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+
+        if not queryset.exists():
+            return Response(
+                {"message": "No posts in this categories found."}, status=200)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
 
 class CategoryDetail(generics.RetrieveUpdateAPIView):
     """

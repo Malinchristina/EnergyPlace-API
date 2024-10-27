328a4341272fafe615acadbf4d1c95c5156e4973
Malinchristina <malin_christina@yahoo.se>
2024-10-27 18:27:03 +0100

    fix(settings): correct misspelling in JWT_AUTH_SECURE configuration

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -42,7 +42,7 @@ if 'DEV' not in os.environ:
     ]
 
 REST_USE_JWT = True
-WT_AUTH_SECURE = True
+JWT_AUTH_SECURE = True
 JWT_AUTH_COOKIE = 'my-app-auth'
 JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
 JWT_AUTH_SAMESITE = 'None'

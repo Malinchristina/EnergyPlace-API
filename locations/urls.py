a1c19aa72a01ccf62e165cc5882f8cebc43714d5
Malinchristina <malin_christina@yahoo.se>
2024-11-15 15:10:27 +0100

    feat(locations): add endpoints for filtered and full country lists

--- locations/urls.py
+++ locations/urls.py
@@ -4,4 +4,9 @@ from locations import views
 urlpatterns = [
     path('locations/', views.LocationList.as_view(), name='locations'),
     path('locations/<int:pk>/', views.LocationDetail.as_view(), name='location'),
+    path('locations/countries/', views.country_list, name='country_list'),
+    path(
+        'locations/full-countries/',
+        views.full_country_list,
+        name='full_country_list'),
 ]

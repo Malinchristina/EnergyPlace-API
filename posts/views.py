a053fc6edcb9b0935dd99e0818d7dae23282237e
Malinchristina <malin_christina@yahoo.se>
2024-11-22 13:30:09 +0100

    feat(posts): add top_liked filter to PostList view for fetching top 5 most liked posts

--- posts/views.py
+++ posts/views.py
@@ -4,8 +4,13 @@ from django_filters.rest_framework import DjangoFilterBackend
 from drf_api.permissions import IsOwnerOrReadOnly
 from .models import Post
 from .serializers import PostSerializer
+from rest_framework.pagination import LimitOffsetPagination
 
 # Create your views here.
+
+class TopFivePagination(LimitOffsetPagination):
+    default_limit = 5  # Only return 5 posts by default
+    max_limit = 5  # Ensure the max limit is 5
 
 class PostList(generics.ListCreateAPIView):
     """
@@ -16,7 +21,10 @@ class PostList(generics.ListCreateAPIView):
     queryset = Post.objects.annotate(
         comments_count=Count('comment', distinct=True),
         likes_count=Count('likes', distinct=True)
-    ).order_by('-likes_count', '-created_at')
+    )
+    # .order_by('-likes_count', '-created_at')
+    # pagination_class = TopFivePagination
+
     filter_backends = [
         filters.OrderingFilter,
         filters.SearchFilter,
@@ -40,6 +48,18 @@ class PostList(generics.ListCreateAPIView):
         'comments_count',
         'likes_count',
     ]
+
+    pagination_class = None  # Default: no pagination
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        if self.request.query_params.get('top_liked') == 'true':
+            # Filter to include only posts with likes_count > 0
+            queryset = queryset.filter(likes_count__gt=0)
+            # Limit the queryset to the top 5 posts based on
+            # likes_count and created_at
+            return queryset.order_by('-likes_count', '-created_at')[:5]
+        return queryset
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)

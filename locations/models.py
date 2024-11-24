34e2f6736164b36270d49d6045af9d7528d4a5d7
Malinchristina <malin_christina@yahoo.se>
2024-11-24 20:09:56 +0100

    feat(locations): update Location model to store only country information

--- locations/models.py
+++ locations/models.py
@@ -5,18 +5,14 @@ from django_countries.fields import CountryField
 
 class Location(models.Model):
     """
-    Location model. Locality is specific location name (e.g., city or landmark). 
+    Location model stores country
     """
-    locality = models.CharField(max_length=100)
     country = CountryField() # Country code e.g. SE
-
-    class Meta:
-        unique_together = ('locality', 'country')
 
     @property
     def country_name(self):
         return self.country.name #Get full country name e.g. Sweden
 
     def __str__(self):
-        # Return only the country name for dropdowns or displays
-        return f"{self.country.name}"
+        # Display country name
+        return self.country.name

5289b921a00ec8a6574dd689ede16594a49104f1
Malinchristina <malin_christina@yahoo.se>
2024-11-24 18:09:40 +0100

    feat(locations): update locations model to separate locality and country

--- locations/models.py
+++ locations/models.py
@@ -10,11 +10,13 @@ class Location(models.Model):
     locality = models.CharField(max_length=100)
     country = CountryField() # Country code e.g. SE
 
+    class Meta:
+        unique_together = ('locality', 'country')
+
     @property
     def country_name(self):
         return self.country.name #Get full country name e.g. Sweden
 
     def __str__(self):
-        if self.locality:
-            return f"{self.locality}, {self.country.name}"
+        # Return only the country name for dropdowns or displays
         return f"{self.country.name}"

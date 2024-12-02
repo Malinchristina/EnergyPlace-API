5488586b50e8ffe7538803af94bc01d602251853
Malinchristina <malin_christina@yahoo.se>
2024-12-02 12:38:21 +0100

    test(posts): add tests for post list, create, and update functionality

--- posts/tests.py
+++ posts/tests.py
@@ -10,24 +10,45 @@ from rest_framework.test import APITestCase
 class PostListviewTest(APITestCase):
     def setUp(self):
         User.objects.create_user(username='anna', password='12345')
+        self.location = Location.objects.create(country="SE")
+        self.category = Category.objects.create(name="Nature")
     
     def test_list_posts(self):
         anna = User.objects.get(username='anna')
-        Post.objects.create(owner=anna, title='Title 1')
+        Post.objects.create(
+            owner=anna,
+            title='Title 1',
+            location=self.location,
+            locality="Stockholm",
+            category=self.category)
         response = self.client.get('/posts/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         print(response.data)
         print(len(response.data))
 
     def test_loggedin_user_can_create_post(self):
+        # Login as the user
         self.client.login(username='anna', password='12345')
-        response = self.client.post('/posts/', {'title': 'Title 1'})
+        
+        # Create a post with all the necessary fields
+        response = self.client.post('/posts/', {
+            'title': 'Title 1',
+            'location_id': self.location.id, 
+            'locality': 'Stockholm',
+            'category_id': self.category.id
+        })
+        
         count = Post.objects.count()
         self.assertEqual(count, 1)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
     def test_not_loggedin_user_cant_create_post(self):
-        response = self.client.post('/posts/', {'title': 'Title 1'})
+        response = self.client.post('/posts/', {
+        'title': 'Title 1',
+        'location_id': self.location.id,
+        'locality': 'Stockholm',
+        'category_id': self.category.id
+    })
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
 
@@ -35,8 +56,25 @@ class PostDetailViewTest(APITestCase):
     def setUp(self):
         anna = User.objects.create_user(username='anna', password='12345')
         bill = User.objects.create_user(username='bill', password='12345')
-        Post.objects.create(owner=anna, title='Title 1', content='Content 1')
-        Post.objects.create(owner=bill, title='Title 2', content='Content 2')
+        # Create location and category for posts
+        self.location = Location.objects.create(country="SE")
+        self.category = Category.objects.create(name="Nature")
+        
+        # Create posts for testing
+        self.post1 = Post.objects.create(
+            owner=anna,
+            title='Title 1',
+            content='Content 1',
+            location=self.location,
+            locality="Stockholm",
+            category=self.category)
+        self.post2 = Post.objects.create(
+            owner=bill,
+            title='Title 2',
+            content='Content 2',
+            location=self.location,
+            locality="Stockholm",
+            category=self.category)
 
     def test_retrieve_post_with_valid_id(self):
         response = self.client.get('/posts/1/')
@@ -49,9 +87,22 @@ class PostDetailViewTest(APITestCase):
 
     def test_loggedin_user_can_update_own_post(self):
         self.client.login(username='anna', password='12345')
-        response = self.client.put('/posts/1/', {'title': 'Title 1 Updated'})
-        post = Post.objects.filter(pk=1).first()
+
+        updated_data = {
+            'title': 'Title 1 Updated',
+            'content': 'Updated content',
+            'location_id': self.location.id,
+            'locality': 'Stockholm',
+            'category_id': self.category.id
+        }
+
+        response = self.client.put(f'/posts/{self.post1.id}/', updated_data)
+        
+        post = Post.objects.get(pk=self.post1.id)
+        
+        # Check if the post is updated correctly
         self.assertEqual(post.title, 'Title 1 Updated')
+        self.assertEqual(post.content, 'Updated content')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_loggedin_user_cant_update_others_post(self):
@@ -59,46 +110,3 @@ class PostDetailViewTest(APITestCase):
         response = self.client.put('/posts/2/', {'title': 'Title 2 Updated'})
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
         
-
-# Test written by me for Category and Location
-class PostCreateUpdateWithCategoryLocationTest(APITestCase):
-    def setUp(self):
-        self.user = User.objects.create_user(username='anna', password='12345')
-        self.client.login(username='anna', password='12345')
-        self.category = Category.objects.create(name='Category 1')
-        self.location = Location.objects.create(name='Location 1')
-
-    def test_create_post_with_category_and_location(self):
-        response = self.client.post('/posts/', {
-            'title': 'Title 1',
-            'content': 'Content 1',
-            'category': 'nature',
-            'location': {'name': self.location.name, 'country': 'US'}
-        },
-        format='json'  # Use JSON format for the request
-        )
-
-        print(response.data)
-
-        post = Post.objects.first()
-        self.assertIsNotNone(post, "Post should have been created")
-        self.assertEqual(post.category.name, 'nature')
-        self.assertEqual(post.location.name, 'Location 1')
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-
-    def test_update_post_with_category_and_location(self):
-        post = Post.objects.create(owner=self.user, title='Title 1', content='Content 1')
-        response = self.client.put(f'/posts/{post.id}/', {
-            'title': 'Title 1 Updated',
-            'content': 'Content 1 Updated',
-            'category': 'nature',
-            'location': {'name': self.location.name, 'country': 'US'}
-        },
-        format='json'  # Use JSON format for the request
-        )
-
-        post.refresh_from_db()
-        self.assertEqual(post.title, 'Title 1 Updated')
-        self.assertEqual(post.category.name, 'nature')
-        self.assertEqual(post.location.name, 'Location 1')
-        self.assertEqual(response.status_code, status.HTTP_200_OK)

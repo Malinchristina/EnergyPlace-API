bc1d819414c653833b4abe16fea7a8cea1b0d883
Malinchristina <malin_christina@yahoo.se>
2024-12-02 17:33:53 +0100

    test: correct location tests for country_name field
    - Fixed assertions for location name in test_list_locations
    - Updated expected country_name values to match the actual response
    - Adjusted filter test to reflect correct country name field

--- locations/tests.py
+++ locations/tests.py
@@ -2,42 +2,42 @@ from rest_framework import status
 from rest_framework.test import APITestCase
 from .models import Location
 from django.contrib.auth.models import User
-
-# Create your tests here.
+from django_countries.fields import CountryField
 
 
 class LocationAPITest(APITestCase):
     def setUp(self):
         self.user = User.objects.create_user(username='anna', password='12345')
         self.client.login(username='anna', password='12345')
-        self.location = Location.objects.create(
-            name='Location 1', country='SE'
-        )
-        self.location2 = Location.objects.create(
-            name='Location 2', country='PS'
-        )
+
+        self.location1 = Location.objects.create(country='SE')
+        self.location2 = Location.objects.create(country='PS')
 
     def test_list_locations(self):
+        """Test that the locations are returned correctly."""
         response = self.client.get('/locations/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), 2)
-        self.assertEqual(response.data[0]['name'], 'Location 1')
-        self.assertEqual(response.data[1]['name'], 'Location 2')
+
+        self.assertEqual(
+            response.data[0]['country_name'], 'Sweden')
+        self.assertEqual(
+            response.data[1]['country_name'], 'Palestine, State of')
 
     def test_list_locations_filter(self):
+        """Test filtering locations by country code."""
         response = self.client.get('/locations/?country=SE')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), 1)
-        self.assertEqual(response.data[0]['name'], 'Location 1')
+
+        self.assertEqual(response.data[0]['country_name'], 'Sweden')
 
     def test_create_location(self):
+        """Test that a new location can be created."""
         response = self.client.post('/locations/', {
-            'name': 'New Location',
             'country': 'IE'
-        },
-            format='json'
-        )
+        }, format='json')
+
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(Location.objects.count(), 3)
-        self.assertEqual(Location.objects.last().name, 'New Location')
-        self.assertEqual(Location.objects.last().country, 'IE')
+        self.assertEqual(Location.objects.last().country.name, 'Ireland')
